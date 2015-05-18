4dc72c9e3972875b3e776d94cb9fa2d29549684e
Norman Rusch <norman.rusch@moccu.com>
2015-05-18 14:34:34 +0200

    Switch to promises

--- example/reporting.js
+++ example/reporting.js
@@ -3,7 +3,7 @@ var
 	 * "node-lintspaces" in your project, require('lintspaces') instead of
 	 * require('../lib/Validator').
 	 */
-	Validator = require('../lib/Validator'),
+	Validator = require('../lib/Runner'),
 	validator = new Validator({
 		newline: true,
 		newlineMaximum: 1,
@@ -11,37 +11,48 @@ var
 		indentation: 'spaces',
 		spaces: 4,
 		ignores: ['js-comments']
-	}),
-	files
+	})
 ;
 
-validator.validate('./reporting.js.example');
-files = validator.getInvalidFiles();
+validator.validate('./reporting.js.example')
+	.then(function() {
+		var files = validator.getInvalidFiles();
 
-// For each invalid file:
-Object.keys(files).forEach(function(file) {
-	var reports = files[file];
+		// For each invalid file:
+		Object.keys(files).forEach(function(file) {
+			var reports = files[file];
 
-	// For each line in file:
-	Object.keys(reports).forEach(function(line) {
-		var errors = reports[line];
+			// For each line in file:
+			Object.keys(reports).forEach(function(line) {
+				var errors = reports[line];
 
-		// A line can contain more than one error, errors are reported in
-		// an array. Loop over these errors:
-		errors.forEach(function(error) {
-			console.error(
-				'Error in Line ' + error.line +
-				' (' + error.code + ' / ' + error.type + ')' +
-				': ' + error.message
-			);
-
-			if (error.payload) {
-				Object.keys(error.payload).forEach(function(payload) {
+				// A line can contain more than one error, errors are reported in
+				// an array. Loop over these errors:
+				errors.forEach(function(error) {
 					console.error(
-						'\t' + payload + ': ' + error.payload[payload]
+						'Error in Line ' + error.line +
+						' (' + error.code + ' / ' + error.type + ')' +
+						': ' + error.message
 					);
+
+					if (error.payload) {
+						Object.keys(error.payload).forEach(function(payload) {
+							console.error(
+								'\t' + payload + ': ' + error.payload[payload]
+							);
+						});
+					}
 				});
-			}
+			});
 		});
+	})
+	.catch(function(error) {
+		console.log('error');
+		console.error(error);
 	});
-});
+
+
+
+
+
+
